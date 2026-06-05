567642a9037978abb720d3a6c4b0f63070141d80
FanJae <hjjang45@gmail.com>
2026-06-05 18:24:00 +0900

    feat : 플레이어 및 리스폰 관련 구현
    - 플레이어의 이동, 점프, 애니메이션과 관련한 Controller 구현
    - 사망 체크 및 캐릭터 리스폰을 진행하는 Controller 구현

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,111 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private enum PlayerState
+    {
+        Idle = 0,
+        Move = 1,
+        Jump = 2
+    }
+    [Header("Move")]
+    [SerializeField] private float moveSpeed = 5.0f;
+
+    [Header("Jump")]
+    [SerializeField] private float jumpPower = 12.0f;
+
+    [Header("Ground")]
+    [SerializeField] private GroundChecker groundChecker;
+
+    [Header("Animation")]
+    [SerializeField] private Animator animator;
+
+    private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
+
+    private PlayerState currentState = PlayerState.Idle;
+
+    
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        if (animator == null)
+            animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        Jump();
+        Flip();
+        UpdateState();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        float moveX = InputManager.Movement.x;
+
+        rb.linearVelocity = new Vector2(moveX * moveSpeed, rb.linearVelocity.y);
+    }
+
+    private void Flip()
+    {
+        float moveX = InputManager.Movement.x;
+
+        if (moveX > 0) spriteRenderer.flipX = false;
+        else if (moveX < 0) spriteRenderer.flipX = true;
+    }
+
+    private void UpdateState()
+    {
+        PlayerState nextState = GetState();
+
+        if (currentState == nextState)
+            return;
+
+        currentState = nextState;
+
+        if (animator != null)
+            animator.SetInteger("State", (int)currentState);
+    }
+
+    private void Jump()
+    {
+        if (!InputManager.IsJump)
+            return;
+
+        Debug.Log("Jump Input");
+
+        if (!groundChecker.IsGrounded)
+        {
+            Debug.Log("Jump Failed: Not Grounded");
+            return;
+        }
+        Debug.Log("Jump Success");
+
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x,jumpPower);
+    }
+
+    private PlayerState GetState()
+    {
+        if (!groundChecker.IsGrounded)
+            return PlayerState.Jump;
+
+        if (Mathf.Abs(InputManager.Movement.x) > 0.01f)
+            return PlayerState.Move;
+
+        return PlayerState.Idle;
+    }
+
+    public void StopMovement()
+    {
+        rb.linearVelocity = Vector2.zero;
+    }
+}
---
+++ Assets/Scripts/Player/RespawnController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class RespawnController : MonoBehaviour
+{
+    [SerializeField] private float fallDeathY = -5f;
+
+    private Vector3 respawnPosition;
+    private PlayerController playerController;
+
+    private void Awake()
+    {
+        playerController = GetComponent<PlayerController>();
+    }
+    private void Start()
+    {
+        respawnPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        CheckFallDeath();
+    }
+
+    private void CheckFallDeath()
+    {
+        if (transform.position.y > fallDeathY) return;
+
+        Respawn();
+    }
+
+    public void SetRespawn(float x)
+    {
+        respawnPosition = new Vector3(x,respawnPosition.y,respawnPosition.z);
+    }
+
+    private void Respawn()
+    {
+        playerController.StopMovement();
+        transform.position = respawnPosition;
+    }
+}
