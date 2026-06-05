de6818fac30dd2dc39b9eeb0b79a00fe7db8d8c6
FanJae <hjjang45@gmail.com>
2026-06-05 18:21:05 +0900

    feat : 플레이어 바닥 체크를 하는 로직 구현
    - 플레이어에 대한 바닥 체크를 하는 로직 구현

---
+++ Assets/Scripts/Player/GroundChecker.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class GroundChecker : MonoBehaviour
+{
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private Vector2 checkSize = new Vector2(0.5f, 0.15f);
+
+    public bool IsGrounded { get; private set; }
+
+    private void FixedUpdate()
+    {
+        IsGrounded = Physics2D.OverlapBox(transform.position,checkSize,0f,groundLayer);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position, checkSize);
+    }
+}
