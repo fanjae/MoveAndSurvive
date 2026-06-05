df6046d677707064c94f26a843dd7874e8dcd97f
FanJae <hjjang45@gmail.com>
2026-06-05 18:20:04 +0900

    feat : 리스폰 위치를 변경해주는 체크포인트 개념을 추가했습니다.
    - 리스폰 위치를 변경해주는 체크포인트 개념을 추가했습니다.

---
+++ Assets/Scripts/Map/CheckPoint.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class CheckPoint : MonoBehaviour
+{
+    private bool isActivated = false;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (isActivated)
+            return;
+
+        if (!other.CompareTag("Player"))
+            return;
+
+        RespawnController respawnController =
+            other.GetComponent<RespawnController>();
+
+        if (respawnController == null)
+            return;
+
+        respawnController.SetRespawn(transform.position.x);
+
+        isActivated = true;
+        Destroy(gameObject);
+    }
+}
