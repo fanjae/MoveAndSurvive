7523bddda27e67a840feea999aa2a7ebb1fb6232
FanJae <hjjang45@gmail.com>
2026-06-05 18:19:36 +0900

    feat : 카메라 워킹 추가
    - 펭귄의 X축을 사이에 두고 이동하는 카메라 워킹을 추가했습니다.

---
+++ Assets/Scripts/Camera/CameraPenguin.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class CameraFollowX : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    private float xOffset;
+
+    private void Awake()
+    {
+        xOffset = transform.position.x - target.position.x;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 pos = transform.position;
+        pos.x = target.position.x + xOffset;
+        transform.position = pos;
+    }
+}
