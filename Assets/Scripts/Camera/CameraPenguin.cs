a071a25ed22b7a8be4a9c7efdb57096e262ced42
FanJae <hjjang45@gmail.com>
2026-06-05 22:48:24 +0900

    feat : 펭귄 위치 따라서 카메라 앞쪽을 보는 형태로 변경
    - 펭귄 위치에 따라 카메라 앞쪽에 보이는 시야 각이 바뀌도록 변경

--- Assets/Scripts/Camera/CameraPenguin.cs
+++ Assets/Scripts/Camera/CameraPenguin.cs
@@ -3,17 +3,20 @@
 public class CameraFollowX : MonoBehaviour
 {
     [SerializeField] private Transform target;
-    private float xOffset;
+    [SerializeField] private SpriteRenderer targetSprite;
 
-    private void Awake()
-    {
-        xOffset = transform.position.x - target.position.x;
-    }
+    [SerializeField] private float lookAheadDistance = 2f;
 
     private void LateUpdate()
     {
         Vector3 pos = transform.position;
-        pos.x = target.position.x + xOffset;
+
+        // 펭귄의 위치에 따라서, 카메라의 보는 앞쪽을 변형할 목적
+        float direction = targetSprite.flipX ? -1f : 1f;
+
+        // 보이는 거리
+        pos.x = target.position.x + direction * lookAheadDistance;
+
         transform.position = pos;
     }
 }
