3e03d5be9217aa034614b59376679cc93202671b
FanJae <hjjang45@gmail.com>
2026-06-05 18:20:49 +0900

    feat : 플레이어 입력을 받는 Input Manager 구현
    - 플레이어의 입력을 받는 InputManager 를 추가하였습니다.

---
+++ Assets/Scripts/Player/InputManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    public static Vector2 Movement { get; private set; } = Vector2.zero;
+    public static bool IsJump { get; private set; } = false;
+
+    private InputAction moveAction;
+    private InputAction jumpAction;
+
+    private void Awake()
+    {
+        moveAction = InputSystem.actions.FindAction("Move");
+        jumpAction = InputSystem.actions.FindAction("Jump");
+    }
+
+    void Update()
+    {
+        Movement = moveAction.ReadValue<Vector2>();
+
+        IsJump = jumpAction.WasPressedThisFrame();
+    }
+}
