559f49967653df0bb92171c2926dfde70d7736da
FanJae <hjjang45@gmail.com>
2026-06-06 17:59:18 +0900

    feat : 별을 먹으면 점수를 얻도록 처리
    - 별을 먹으면 점수를 얻을 수 있도록 처리했습니다.

---
+++ Assets/Scripts/StarItem.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class StarItem : MonoBehaviour
+{
+    [SerializeField] private int scoreValue = 1;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.CompareTag("Player"))
+            return;
+
+        ScoreManager.Instance.AddScore(scoreValue);
+
+        Destroy(gameObject);
+    }
+}
