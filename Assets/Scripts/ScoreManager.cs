06bd92bb0cb39e9da84eeca29641b3c71f65bfec
FanJae <hjjang45@gmail.com>
2026-06-06 22:31:31 +0900

    comment : 주석 추가
    - 주석을 추가했습니다.

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -8,6 +8,7 @@ public class ScoreManager : MonoBehaviour
     [SerializeField] private TextMeshProUGUI scoreText;
 
     private int score = 0;
+    public int Score => score;
 
     private void Awake()
     {
@@ -19,16 +20,16 @@ public class ScoreManager : MonoBehaviour
         UpdateUI();
     }
 
-    public void AddScore(int value)
+    public void AddScore(int value) // 점수 증가
     {
         score += value;
         UpdateUI();
     }
 
-    private void UpdateUI()
+    private void UpdateUI() // 점수 업데이트
     {
         scoreText.text = $"x {score}";
     }
 
-    public int Score => score;
+    
 }
