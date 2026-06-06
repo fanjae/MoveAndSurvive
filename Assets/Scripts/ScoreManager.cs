86fb0bb4b56573925efb250f613f3e49dc05e1a8
FanJae <hjjang45@gmail.com>
2026-06-06 17:55:45 +0900

    feat : Score UI 추가
    - 별을 먹으면 화면상 오른쪽 화면에 점수가 반영되도록 추가했습니다.

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,34 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager Instance { get; private set; }
+
+    [SerializeField] private TextMeshProUGUI scoreText;
+
+    private int score = 0;
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+
+        UpdateUI();
+    }
+
+    public void AddScore(int value)
+    {
+        score += value;
+        UpdateUI();
+    }
+
+    private void UpdateUI()
+    {
+        scoreText.text = $"x {score}";
+    }
+
+    public int Score => score;
+}
