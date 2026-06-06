eb70c42d60d7ff6e77ff37cdd9cd9b3aa1899918
FanJae <hjjang45@gmail.com>
2026-06-06 17:55:19 +0900

    feat : 클리어 로직 추가
    - 클리어 시 화면에 UI 패널이 나오고, 종료되도록 추가했습니다.

---
+++ Assets/Scripts/Map/ClearGoal.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ClearGoal : MonoBehaviour
+{
+    [SerializeField] private GameObject clearPanel;
+
+    private InputAction exitAction;
+    private bool isCleared = false;
+
+    private void Awake()
+    {
+        exitAction = InputSystem.actions.FindAction("Exit");
+
+        if (exitAction == null) return;
+    }
+
+    private void Start()
+    {
+        clearPanel.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (!isCleared) return;
+        if (exitAction == null) return;
+
+        if (exitAction.WasPressedThisFrame())
+        {
+            QuitGame();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (isCleared) return;
+        if (!other.CompareTag("Player")) return;
+
+        isCleared = true;
+        clearPanel.SetActive(true);
+
+        Time.timeScale = 0f;
+    }
+
+    private void QuitGame()
+    {
+        Time.timeScale = 1f;
+
+        Application.Quit();
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+}
