d695878fd5218d866f6d9fedea78086a3b8cd6a7
FanJae <hjjang45@gmail.com>
2026-06-06 17:52:36 +0900

    feat : 카메라 이동을 매끄럽게 가도록 처리.
    - 카메라 이동을 Mathf.SommotDamp를 이용해서 조금 더 자연스럽게 이동하도록 구현

--- Assets/Scripts/Camera/CameraPenguin.cs
+++ Assets/Scripts/Camera/CameraPenguin.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class CameraFollowX : MonoBehaviour
 {
@@ -6,6 +7,9 @@ public class CameraFollowX : MonoBehaviour
     [SerializeField] private SpriteRenderer targetSprite;
 
     [SerializeField] private float lookAheadDistance = 2f;
+    [SerializeField] private float smoothTime = 0.25f;
+
+    private float velocityX;
 
     private void LateUpdate()
     {
@@ -13,9 +17,10 @@ public class CameraFollowX : MonoBehaviour
 
         // 펭귄의 위치에 따라서, 카메라의 보는 앞쪽을 변형할 목적
         float direction = targetSprite.flipX ? -1f : 1f;
+        float targetX = target.position.x + direction * lookAheadDistance;
 
         // 보이는 거리
-        pos.x = target.position.x + direction * lookAheadDistance;
+        pos.x = Mathf.SmoothDamp(pos.x,targetX,ref velocityX,smoothTime);
 
         transform.position = pos;
     }
